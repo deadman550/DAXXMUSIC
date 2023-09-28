9cb4cb5718306fe15416f6e5b1a11c61b1b35644
KingEvil55 <98324609+KingEvil55@users.noreply.github.com>
2023-09-28 09:07:10 +0530

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -5,63 +5,29 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 def help_pannel(_, START: Union[bool, int] = None):
     first = [
-        InlineKeyboardButton(
-            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
-        )
+        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"),
     ]
     second = [
-        InlineKeyboardButton(
-            text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
-        ),
-        InlineKeyboardButton(
-            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
-        ),
+        InlineKeyboardButton(text=_["BACK_BUTTON"],callback_data=f"settingsback_helper",),
+        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data=f"close"),
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text="Aᴅᴍɪɴ",
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text="Aᴜᴛʜ",
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text="Bʟɪsᴛ",
-                    callback_data="help_callback hb3",
-                ),
+                InlineKeyboardButton(text="Aᴅᴍɪɴ",callback_data="help_callback hb1",),
+                InlineKeyboardButton(text="Aᴜᴛʜ",callback_data="help_callback hb2",),
+                InlineKeyboardButton(text="Bʟɪsᴛ",callback_data="help_callback hb3",),
             ],
             [
-                InlineKeyboardButton(
-                    text="Bᴄᴀsᴛ",
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text="Gʙᴀɴ",
-                    callback_data="help_callback hb12",
-                ),
-                InlineKeyboardButton(
-                    text="Lʏʀɪᴄs",
-                    callback_data="help_callback hb5",
-                ),
+                InlineKeyboardButton(text="Bᴄᴀsᴛ",callback_data="help_callback hb4",),
+                InlineKeyboardButton(text="Gʙᴀɴ",callback_data="help_callback hb12",),
+                InlineKeyboardButton(text="Lʏʀɪᴄs",callback_data="help_callback hb5",),
             ],
             [
-                InlineKeyboardButton(
-                    text="Pɪɴɢ",
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text="Pʟᴀʏ",
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text="Pʟɪsᴛ",
-                    callback_data="help_callback hb6",
-                ),
+                InlineKeyboardButton(text="Pɪɴɢ",callback_data="help_callback hb7",),
+                InlineKeyboardButton(text="Pʟᴀʏ",callback_data="help_callback hb8",),
+                InlineKeyboardButton(text="Pʟɪsᴛ",callback_data="help_callback hb6",),
             ],
             [
                 InlineKeyboardButton(text="Vᴄʜᴀᴛs",callback_data="help_callback hb10",),
@@ -78,14 +44,9 @@ def help_back_markup(_):
     upl = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
-                ),
+                InlineKeyboardButton(text=_["BACK_BUTTON"],callback_data=f"settings_back_helper",),
                 
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"], callback_data=f"close"
-                )
+                InlineKeyboardButton(text=_["CLOSE_BUTTON"],callback_data=f"close"),
             ]
         ]
     )
@@ -95,10 +56,7 @@ def help_back_markup(_):
 def private_help_panel(_):
     buttons = [
         [
-            InlineKeyboardButton(
-                text="Hᴇʟᴘ",
-                callback_data="settings_back_helper",
-            ),
+            InlineKeyboardButton(text="Hᴇʟᴘ",callback_data="settings_back_helper",),
         ],
     ]
     return buttons
