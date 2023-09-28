82f0564001c7a8b458cc2b6ee0e3fba985abfd2d
KingEvil55 <98324609+KingEvil55@users.noreply.github.com>
2023-09-28 09:12:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,8 +25,8 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/KingEvil55/DAXXMUSIC
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Cyberdaxx")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Cyberdaxxx")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/miss_x_network")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/themortalclan")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
