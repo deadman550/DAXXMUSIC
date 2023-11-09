0b5140e6d553796a88b374818b622d79e78e46fd
KingEvil55 <98324609+KingEvil55@users.noreply.github.com>
2023-11-09 09:25:46 +0530

    Update tagall.py

--- AnonX/plugins/tagall.py
+++ AnonX/plugins/tagall.py
@@ -91,7 +91,7 @@ async def mentionall(client, message):
 
         if usrnum == 1:
             if mode == "text_on_cmd":
-                txt = f"{msg}\n{usrtxt}"
+                txt = f"msg\n{usrtxt}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
