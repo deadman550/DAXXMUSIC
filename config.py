974306c966c1daae12bfc7099c566c05412f0925
KingEvil55 <98324609+KingEvil55@users.noreply.github.com>
2023-11-21 13:45:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/miss_x_network")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/themortalclan")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/check_my_groups01")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
